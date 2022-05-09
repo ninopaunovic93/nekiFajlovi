a279e494e45e7b902725036edd997bfc76bab369
nino <ninopaunov93@outlook.com>
2022-05-09 22:27:11 +0200

    some shanges

--- dom.js
+++
@@ -1,5 +0,0 @@
-export const divIspisKorisnika = document.createElement('div')
-divIspisKorisnika.classList.add('container')
-
-export const body = document.body
-body.appendChild(divIspisKorisnika)
--- main.js
+++
@@ -1,9 +0,0 @@
-import { renderBase } from "./renderBase.js"
-import { renderUser } from "./renderUser.js"
-import { korisnici } from "./objekat.js"
-import { divIspisKorisnika} from "./dom.js"
-import { body } from "./dom.js"
-
-
-renderBase()
-renderUser()
